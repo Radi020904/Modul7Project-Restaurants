cf3db561f83779330e94a06be6696065504a5143
Radi020904 <radimarch@gmail.com>
2022-03-17 16:47:38 +0200

    Menu and MenuMeal classes

---
+++ Module7_project_Restaurants/Data/Models/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7_project_Restaurants.Data.Models
+    public class Menu
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Range(typeof(double), "1.00", "10.00")]
+        public double Rating { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "meal name is too long!")]
+        public string Type { get; set; }       
+        [Required]
+        [MaxLength(50, ErrorMessage = "meal type is too long")]
+        public string Language { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "meal type is too long")]
+        public string Link { get; set; }
+        public override string ToString()
+        {
+            string result = "Meal:\n";
+            result += $"rating: {Rating}\n";
+            result += $"type: {Type}\n";
+            result += $"language: {Language}";
+            result += $"link: {Link}";
+            return result;
+        }
+    }
+}
---
+++ Module7_project_Restaurants/Data/Models/MenuMeal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7_project_Restaurants.Data.Models
+{
+    public class MenuMeal
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        [ForeignKey(nameof(Meal))]
+        public int MealId { get; set; }
+        [ForeignKey(nameof(Menu))]
+        public int MenuId { get; set; }
+        public virtual Meal Meal { get; set; }
+        public virtual Menu Menu { get; set; }
+        public override string ToString()
+        {
+            string result = "MenuMeal:\n";
+            result += $"MealID: {MealId}\n";
+            result += $"MenuId: {MenuId}\n";
+            return result;
+        }
+    }
+}
